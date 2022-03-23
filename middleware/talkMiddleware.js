dcafd0eb19a2111525af295687d031af74eaca20
leandrochs <leandrochagask@gmail.com>
2022-03-23 00:25:53 -0300

    fix: remove exclamacao de rate e coloca undefined O valor de rate igual a zero nao passou pelo if Foi necessario colocar undefined de forma explicita

--- middleware/talkMiddleware.js
+++ middleware/talkMiddleware.js
@@ -1,5 +1,9 @@
 function talkMiddleware(req, res, next) {
-  if (!req.body.talk || !req.body.talk.watchedAt || !req.body.talk.rate) {
+  if (
+    !req.body.talk
+    || !req.body.talk.watchedAt
+    || req.body.talk.rate === undefined
+  ) {
     return res.status(400).json({
       message:
         'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
