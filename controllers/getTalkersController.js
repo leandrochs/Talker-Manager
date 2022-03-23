fc18d1d60f60e619055c94ccf91132b926263501
leandrochs <leandrochagask@gmail.com>
2022-03-23 11:37:32 -0300

    emprove: insere if para identificar mensagem de erro

--- controllers/getTalkersController.js
+++ controllers/getTalkersController.js
@@ -3,10 +3,15 @@ const readFileFunc = require('../services/readFileFunc');
 async function getTalkersController(req, res) {
   try {
     const data = await readFileFunc('./talker.json');
+
+    if (data.messageError) {
+      return res.status(400).json({ message: data.messageError });
+    }
+
     return res.status(200).json(data);
   } catch (error) {
     console.log(`Não foi possível ler arquivo. Erro: ${error.message}`);
-    return res.status(401).json({ message: `Erro no app: ${error}` });
+    return res.status(401).json({ message: `Erro no app: ${error.message}` });
   }
 }
 
